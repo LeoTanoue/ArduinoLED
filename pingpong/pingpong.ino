9a9f30120aa47788defb32d04bafabd1b5c8550d
Leonardo Tanoue <leo.tanoue@gmail.com>
2017-02-01 22:48:36 -0600

    updating ping pong

--- pingpong/pingpong.ino
+++ pingpong/pingpong.ino
@@ -1,5 +1,4 @@
-#include <WS2812.h>
-#include <cRGB.h>
+#include <LEDColors.h>
 
 #define outputPin 9  // Digital output pin (default: 9)
 #define LEDCount 60   // Number of LEDs to drive (default: 9)
@@ -19,12 +18,12 @@ boolean firstTime = true;
 void loop()
 {  
   int delayValue = 25;
-  int i,r,g,b;
+  int i;
   
   if(firstTime)
   {
     //Blank
-    ChangeRGB(0,0,0);
+    value = Blank();
     for(i = 0; i < LEDCount; i++)
     {
         LED.set_crgb_at(i, value);
@@ -34,52 +33,33 @@ void loop()
     delay(delayValue); // Wait (ms)
   }
   
-  r = getRandomColor();
-  g = getRandomColor();
-  b = getRandomColor();
+  value = RandomColor();
   for(i = 0; i <= LEDCount; i++)
   {
-    ChangeRGB(r,g,b);
     LED.set_crgb_at(i, value);
     LED.set_crgb_at(i+1, value);
-    ChangeRGB(0,0,0);
     if( i > 1)
     {
-      LED.set_crgb_at(i-1,value);
-      LED.set_crgb_at(i-2,value);
+      LED.set_crgb_at(i-1,Blank());
+      LED.set_crgb_at(i-2,Blank());
     }
     LED.sync();
     delay(delayValue);
   }
 
-  r = getRandomColor();
-  g = getRandomColor();
-  b = getRandomColor();
+  
+  value = RandomColor();
   for(i = LEDCount; i > 0; i--)
   {
-    ChangeRGB(r,g,b);
     LED.set_crgb_at(i, value);
     LED.set_crgb_at(i-1, value);
-    ChangeRGB(0,0,0);
     if( i < (LEDCount - 2))
     {
-      LED.set_crgb_at(i+1,value);
-      LED.set_crgb_at(i+2,value);
+      LED.set_crgb_at(i+1,Blank());
+      LED.set_crgb_at(i+2,Blank());
     }
     LED.sync();
     delay(delayValue);
   }
   
-}
-
-void ChangeRGB(int r,int g,int b)
-{
-  value.r = r;
-  value.g = g;
-  value.b = b;
-}
-
-int getRandomColor()
-{
-  return random(0,256);
 }
