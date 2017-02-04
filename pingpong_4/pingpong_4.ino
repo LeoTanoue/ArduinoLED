49f2992eacfc17dfcb152b1b2b2490b44a7f5570
Leonardo Tanoue <leo.tanoue@gmail.com>
2017-02-03 18:55:53 -0600

    fixing some code

--- pingpong_4/pingpong_4.ino
+++ pingpong_4/pingpong_4.ino
@@ -1,15 +1,15 @@
-#include <WS2812.h>
-#include <cRGB.h>
+#include <LEDColors.h>
 
-#define outputPin 9  // Digital output pin (default: 9)
-#define LEDCount 60   // Number of LEDs to drive (default: 9)
+#define outputPin 9    // Digital output pin (default: 9)
+#define LEDCount 60    // Number of LEDs to drive (default: 9)
+#define ballSize 10     // LED cluster size
+#define delayValue 10  // Delay value in milliseconds
 
 //List of LED's
 WS2812 LED1(LEDCount);
 WS2812 LED2(LEDCount);
 WS2812 LED3(LEDCount);
 WS2812 LED4(LEDCount);
-cRGB value;
 
 void setup()
 {
@@ -18,59 +18,52 @@ void setup()
   LED2.setOutput(outputPin); 
   LED3.setOutput(outputPin); 
   LED4.setOutput(outputPin);
+  Clear();
 }
-
-boolean firstTime = true;
 
 void loop()
 {  
-  int delayValue = 20;
-  int i,r,g,b;
-  
-  if(firstTime)
+  Ping();
+  Pong();
+}
+
+void Clear()
+{
+  cRGB value = Blank();
+  for(int i = 0; i < LEDCount; i++)
   {
-    //Blank
-    ChangeRGB(0,0,0);
-    for(i = 0; i < LEDCount; i++)
-    {
-        LED1.set_crgb_at(i, value);
-        LED2.set_crgb_at(i, value);
-        LED3.set_crgb_at(i, value);
-        LED4.set_crgb_at(i, value);
-    }
-    LED1.sync(); // Sends the data to the LEDs
-    LED2.sync();
-    LED3.sync();
-    LED4.sync();
-    firstTime = false;
-    delay(delayValue); // Wait (ms)
-  }
-  
-  r = getRandomColor();
-  g = getRandomColor();
-  b = getRandomColor();
-  for(i = 0; i <= LEDCount; i++)
-  {
-    ChangeRGB(r,g,b);
     LED1.set_crgb_at(i, value);
-    LED1.set_crgb_at(i+1, value);
     LED2.set_crgb_at(i, value);
-    LED2.set_crgb_at(i+1, value);
     LED3.set_crgb_at(i, value);
-    LED3.set_crgb_at(i+1, value);
     LED4.set_crgb_at(i, value);
-    LED4.set_crgb_at(i+1, value);
-    ChangeRGB(0,0,0);
-    if( i > 1)
+  }    
+  LED1.sync(); // Sends the data to the LEDs
+  LED2.sync();
+  LED3.sync();
+  LED4.sync();
+}
+
+void Ping()
+{
+  cRGB value = RandomColor();
+  for(int i = 0; i <= LEDCount; i++)
+  {
+    for(int b = 0; b < ballSize; b++)
     {
-      LED1.set_crgb_at(i-1,value);
-      LED1.set_crgb_at(i-2,value);
-      LED2.set_crgb_at(i-1,value);
-      LED2.set_crgb_at(i-2,value);
-      LED3.set_crgb_at(i-1,value);
-      LED3.set_crgb_at(i-2,value);
-      LED4.set_crgb_at(i-1,value);
-      LED4.set_crgb_at(i-2,value);
+      LED1.set_crgb_at(i + b, value);
+      LED2.set_crgb_at(i + b, value);
+      LED3.set_crgb_at(i + b, value);
+      LED4.set_crgb_at(i + b, value);
+    }
+    if(i > ballSize-1)
+    {
+      for(int c = ballSize; c > 0; c--)
+      {
+        LED1.set_crgb_at(i-c,Blank());
+        LED2.set_crgb_at(i-c,Blank());
+        LED3.set_crgb_at(i-c,Blank());
+        LED4.set_crgb_at(i-c,Blank());
+      }
     }
     LED1.sync();
     LED2.sync();
@@ -78,32 +71,29 @@ void loop()
     LED4.sync();
     delay(delayValue);
   }
+}
 
-  r = getRandomColor();
-  g = getRandomColor();
-  b = getRandomColor();
-  for(i = LEDCount; i > 0; i--)
+void Pong()
+{
+  cRGB value = RandomColor();
+  for(int i = LEDCount; i > 0; i--)
   {
-    ChangeRGB(r,g,b);
-    LED1.set_crgb_at(i, value);
-    LED1.set_crgb_at(i-1, value);
-    LED2.set_crgb_at(i, value);
-    LED2.set_crgb_at(i-1, value);
-    LED3.set_crgb_at(i, value);
-    LED3.set_crgb_at(i-1, value);
-    LED4.set_crgb_at(i, value);
-    LED4.set_crgb_at(i-1, value);
-    ChangeRGB(0,0,0);
-    if( i < (LEDCount - 2))
+    for(int b = 0; b < ballSize; b++)
     {
-      LED1.set_crgb_at(i+1,value);
-      LED1.set_crgb_at(i+2,value);
-      LED2.set_crgb_at(i+1,value);
-      LED2.set_crgb_at(i+2,value);
-      LED3.set_crgb_at(i+1,value);
-      LED3.set_crgb_at(i+2,value);
-      LED4.set_crgb_at(i+1,value);
-      LED4.set_crgb_at(i+2,value);
+      LED1.set_crgb_at(i - b, value);
+      LED2.set_crgb_at(i - b, value);
+      LED3.set_crgb_at(i - b, value);
+      LED4.set_crgb_at(i - b, value);
+    }
+    if(i < (LEDCount - ballSize))
+    {
+      for(int c = ballSize; c > 0; c--)
+      {
+        LED1.set_crgb_at(i+c,Blank());
+        LED2.set_crgb_at(i+c,Blank());
+        LED3.set_crgb_at(i+c,Blank());
+        LED4.set_crgb_at(i+c,Blank());
+      }
     }
     LED1.sync();
     LED2.sync();
@@ -111,17 +101,4 @@ void loop()
     LED4.sync();
     delay(delayValue);
   }
-  
-}
-
-void ChangeRGB(int r,int g,int b)
-{
-  value.r = r;
-  value.g = g;
-  value.b = b;
-}
-
-int getRandomColor()
-{
-  return random(0,256);
 }
